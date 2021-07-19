cb07a78aed63a771860e93d077421c6a6a530bdf
CyrivlClth <cyrivlclth@live.com>
2021-07-19 22:03:31 +0800

    feat(interactive-cli): tomato clock cli
    This is a very early version.

--- cmd/gomato/main.go
+++ cmd/gomato/main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"context"
+	"os"
+
+	"github.com/go-gosh/tomato/app"
+	"github.com/go-gosh/tomato/app/ent"
+	"github.com/go-gosh/tomato/app/ent/migrate"
 	"github.com/go-gosh/tomato/app/interactivecli"
+	"github.com/go-gosh/tomato/app/service"
+	_ "github.com/mattn/go-sqlite3"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
-	a := &interactivecli.MainView{}
-	err := a.Run()
+	cb, err := os.Open("./config/config.yaml")
+	if err != nil {
+		panic(err)
+	}
+	cf := app.Config{}
+	err = yaml.NewDecoder(cb).Decode(&cf)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := ent.Open(cf.Database.Type, cf.Database.File)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Schema.Create(context.Background(), migrate.WithForeignKeys(false))
+	if err != nil {
+		panic(err)
+	}
+
+	svc := service.New(db)
+	a := interactivecli.NewMainView(db, svc)
+	err = a.Run()
 	if err != nil {
 		panic(err)
 	}
