50546db072a559837505ef6578581676b5907030
CyrivlClth <cyrivlclth@live.com>
2021-08-25 21:06:16 +0800

    feat: update alert for windows
    async alert and show a message box dialog in windows now.

--- app/minicli/adapter/alarm_windows.go
+++ app/minicli/adapter/alarm_windows.go
@@ -18,9 +18,20 @@ func (d dataManager) Alarm() {
 	funName := "PlaySound"
 	// register handle
 	win32Fun, err := syscall.GetProcAddress(syscall.Handle(funInDllFile), funName)
-	// call function to play sound 3 times
-	for i := 0; i < 3; i++ {
-		ptr, _ := syscall.UTF16PtrFromString("alert")
-		_, _, err = syscall.Syscall6(win32Fun, 3, uintptr(unsafe.Pointer(ptr)), uintptr(0), uintptr(0), 0, 0, 0)
-	}
+	go showMessageBox("gomato", "You have completed a tomato!")
+	go func() {
+		// call function to play sound 3 times
+		for i := 0; i < 3; i++ {
+			ptr, _ := syscall.UTF16PtrFromString("alert")
+			_, _, err = syscall.Syscall6(win32Fun, 3, uintptr(unsafe.Pointer(ptr)), uintptr(0), uintptr(0), 0, 0, 0)
+		}
+	}()
+}
+
+func showMessageBox(title, text string) {
+	user32 := syscall.NewLazyDLL("user32.dll")
+	proc := user32.NewProc("MessageBoxW")
+	textPtr, _ := syscall.UTF16PtrFromString(text)
+	titlePtr, _ := syscall.UTF16PtrFromString(title)
+	proc.Call(uintptr(0), uintptr(unsafe.Pointer(textPtr)), uintptr(unsafe.Pointer(titlePtr)), uintptr(0))
 }
