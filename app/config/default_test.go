bddcfee784bf732151f1b907375c60014a3c01a5
CyrivlClth <cyrivlclth@live.com>
2021-08-25 21:44:11 +0800

    test: compare default config bytes and example yaml file

---
+++ app/config/default_test.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_defaultConfigSource(t *testing.T) {
+	fp, err := ioutil.ReadFile("./metadata/config.example.yaml")
+	require.NoError(t, err)
+	t.Logf("%s", fp)
+	require.Equal(t, fp, defaultConfigSource)
+}
